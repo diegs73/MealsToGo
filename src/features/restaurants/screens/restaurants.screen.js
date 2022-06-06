5e4995c463f10cb4d2904a6236da3688e6b74644
diegs73 <diego.taviano@tin.it>
2022-06-06 17:44:58 +0200

    Add: 0.2

--- src/features/restaurants/screens/restaurants.screen.js
+++ src/features/restaurants/screens/restaurants.screen.js
@@ -1,31 +1,41 @@
-import React from "react";
+import React, { useContext } from "react";
 import { Searchbar } from "react-native-paper";
 import styled from "styled-components/native";
-import { StatusBar, View, SafeAreaView } from "react-native";
+import { View, FlatList } from "react-native";
+import { SafeArea } from "../../../components/utility/safe-area.component";
 import { RestauranInfoCard } from "./../components/restaurant-info-card.component";
-
-const SafeArea = styled(SafeAreaView)`
-  flex: 1;
-  margin-top: ${StatusBar.currentHeight}px;
-`;
+import { Spacer } from "../../../components/spacer/spacer.component";
+import { RestaurantsContext } from "./../../../services/restaurants/restaurants.context";
 
 const SearchContainer = styled(View)`
-  padding: 16px;
+  padding: ${(props) => props.theme.space[3]}; ;
 `;
 
-const RestaurantListContainer = styled(View)`
-  flex: 1;
-  padding: 16px;
-  background-color: blue;
-`;
+const RestaurantList = styled(FlatList).attrs({
+  contentContainerStyle: {
+    padding: 16,
+  },
+})``;
 
-export const RestaurantsScreen = () => (
-  <SafeArea>
-    <SearchContainer>
-      <Searchbar placeholder="Search" />
-    </SearchContainer>
-    <RestaurantListContainer>
-      <RestauranInfoCard />
-    </RestaurantListContainer>
-  </SafeArea>
-);
+export const RestaurantsScreen = () => {
+  const { isLoading, error, restaurants } = useContext(RestaurantsContext);
+
+  return (
+    <SafeArea>
+      <SearchContainer>
+        <Searchbar placeholder="Search" />
+      </SearchContainer>
+      <RestaurantList
+        data={restaurants}
+        renderItem={({ item }) => {
+          return (
+            <Spacer position="bottom" size="large">
+              <RestauranInfoCard restaurant={item} />
+            </Spacer>
+          );
+        }}
+        keyExtractor={(item) => item.name}
+      />
+    </SafeArea>
+  );
+};
